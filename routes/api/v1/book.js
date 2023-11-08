e6a236670a6e184442b46e73bcb283248580027d
RAHUL KUMAR <rahulvaidya148@gmail.com>
2023-11-08 23:30:29 +0530

    commit :: implemented authentication

--- routes/api/v1/book.js
+++ routes/api/v1/book.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const authController = require("../../../controllers/authController");
 
 const router = express.Router();
 
 const booksApi = require("../../../controllers/bookController");
 
-router.route("/").post(booksApi.addNewBook).get(booksApi.getAllBooks);
+router
+  .route("/")
+  .post(authController.protect, booksApi.addNewBook)
+  .get(booksApi.getAllBooks);
 
+router.use(authController.protect);
 router
   .route("/:id")
   .get(booksApi.getBook)
--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -3,5 +3,6 @@ const express = require("express");
 const router = express.Router();
 
 router.use("/books", require("./book"));
+router.use("/users", require("./user"));
 
 module.exports = router;
